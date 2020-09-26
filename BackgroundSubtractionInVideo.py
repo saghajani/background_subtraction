0295ca7b4ce7d2a1d22adb0bd63c01440a4246d3
Saeed Aghajani <saghajani93@gmail.com>
2020-09-27 00:49:22 +0330

    first commit

---
+++ BackgroundSubtractionInVideo.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+
+# Capture a video by webcam
+cap = cv2.VideoCapture(0)
+# Or Capture from a saved file
+# cap = cv2.VideoCapture('output2.mp4')
+# Create BackgroundSubtractor object
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'MP42') #DIVX XVID
+out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        fgmask = fgbg.apply(frame)
+
+        out.write(fgmask)
+
+        cv2.imshow('Original',frame)
+        cv2.imshow('Background Removed',fgmask)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
